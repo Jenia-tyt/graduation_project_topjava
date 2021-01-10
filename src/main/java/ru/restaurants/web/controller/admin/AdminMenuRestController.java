1845bbd3b69cd9e5cf5c207d5dfdceab273f3d0c
Jenia-tyt <uvarovevgen1991@gamil.com>
2021-01-10 21:23:49 +0300

    Validation uqnicom email and name finished

--- src/main/java/ru/restaurants/web/controller/admin/AdminMenuRestController.java
+++ src/main/java/ru/restaurants/web/controller/admin/AdminMenuRestController.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.restaurants.model.Menu;
 import ru.restaurants.service.MenuService;
+import ru.restaurants.service.RestaurantService;
+import ru.restaurants.to.ToMenu;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.time.LocalDate;
 import java.util.List;
+
+import static ru.restaurants.util.Convector.covertToMenu;
 
 @RestController
 @RequestMapping(value = AdminMenuRestController.ADMIN_MENU_TO_DAY, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -22,8 +27,12 @@ public class AdminMenuRestController {
     @Autowired
     private final MenuService service;
 
-    public AdminMenuRestController(MenuService service) {
+    @Autowired
+    private final RestaurantService restaurantService;
+
+    public AdminMenuRestController(MenuService service, RestaurantService restaurantService) {
         this.service = service;
+        this.restaurantService = restaurantService;
     }
 
     @GetMapping
@@ -44,19 +53,18 @@ public class AdminMenuRestController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Menu> create (@RequestBody Menu m){
-        Menu menu = service.save(m);
+    public ResponseEntity<Menu> create (@RequestBody @Valid ToMenu toMenu){
+        Menu menu = service.save(covertToMenu(toMenu, restaurantService));
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path(ADMIN_MENU_TO_DAY + "/{id}")
-                .buildAndExpand(m.getId()).toUri();
-
+                .buildAndExpand(menu.getId()).toUri();
         return ResponseEntity.created(uriOfNewResource).body(menu);
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    public void upDate(@RequestBody Menu m, @PathVariable int id){
-        service.upDate(m, id);
+    public void upDate(@RequestBody @Valid ToMenu toMenu, @PathVariable int id){
+        Menu menu = covertToMenu(toMenu, restaurantService);
+        service.upDate(menu, id);
     }
-
 }
