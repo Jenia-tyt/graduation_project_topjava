9722dfd38cc31c00ced1a1f4b47ba90e5bef83a9
Jenia-tyt <uvarovevgen1991@gamil.com>
2021-01-21 15:58:09 +0300

    Test for root controllers is finished

--- src/test/java/ru/restaurants/web/TestUtil.java
+++ src/test/java/ru/restaurants/web/TestUtil.java
@@ -1,5 +1,6 @@
 package ru.restaurants.web;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -29,5 +30,9 @@ public class TestUtil {
 
     public static RequestPostProcessor userHttpBasic(User user) {
         return SecurityMockMvcRequestPostProcessors.httpBasic(user.getEmail(), user.getPassword());
+    }
+
+    public static RequestPostProcessor userAuth(User user) {
+        return SecurityMockMvcRequestPostProcessors.authentication(new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
     }
 }
--- src/test/java/ru/restaurants/web/controller/RootControllerTest.java
+++ src/test/java/ru/restaurants/web/controller/RootControllerTest.java
@@ -1,44 +1,91 @@
 package ru.restaurants.web.controller;
 
 import org.junit.jupiter.api.Test;
+import ru.restaurants.web.AbstractControllerTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.restaurants.repository.UserDataTest.USER_WITH_ID_16;
+import static ru.restaurants.web.TestUtil.userAuth;
 
-class RootControllerTest {
+class RootControllerTest extends AbstractControllerTest {
 
     @Test
-    void root() {
+    void root() throws Exception {
+        perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"))
+                .andDo(print());
     }
 
     @Test
-    void login() {
+    void login() throws Exception {
+        perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/login.jsp"))
+                .andDo(print());
     }
 
     @Test
-    void logout() {
+    void logout() throws Exception {
+        perform(get("/logout"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"))
+                .andDo(print());
     }
 
     @Test
-    void users() {
+    void users() throws Exception {
+        perform(get("/users")
+                .with(userAuth(USER_WITH_ID_16)))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andDo(print());
     }
 
     @Test
-    void restaurant() {
+    void restaurant() throws Exception {
+        perform(get("/restaurant")
+                .with(userAuth(USER_WITH_ID_16)))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/restaurant.jsp"))
+                .andDo(print());
     }
 
     @Test
-    void menuToDay() {
+    void menuToDay() throws Exception {
+        perform(get("/menuToDay")
+                .with(userAuth(USER_WITH_ID_16)))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/menuToDay.jsp"))
+                .andDo(print());
     }
 
     @Test
-    void menusOfRestaurant() {
+    void menusOfRestaurant() throws Exception {
+        perform(get("/menusOfRestaurant")
+                .with(userAuth(USER_WITH_ID_16)))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/menusOfRestaurant.jsp"))
+                .andDo(print());
     }
 
     @Test
-    void register() {
+    void register() throws Exception {
+        perform(get("/register")
+                .with(userAuth(USER_WITH_ID_16)))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/register.jsp"))
+                .andDo(print());
     }
 
     @Test
-    void profile() {
+    void profile() throws Exception {
+        perform(get("/profile")
+                .with(userAuth(USER_WITH_ID_16)))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/profile.jsp"))
+                .andDo(print());
     }
 }
