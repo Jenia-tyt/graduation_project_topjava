87342bc7ba73cab65ae1bbce85857a727b33f19c
Jenia-tyt <uvarovevgen1991@gamil.com>
2021-01-23 01:21:23 +0300

    REST app is finished

--- src/test/java/ru/restaurants/web/controller/ProfileRestControllerTest.java
+++ src/test/java/ru/restaurants/web/controller/ProfileRestControllerTest.java
@@ -6,15 +6,17 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import ru.restaurants.model.User;
 import ru.restaurants.service.UserService;
+import ru.restaurants.service.VoteService;
 import ru.restaurants.util.execption.ErrorType;
 import ru.restaurants.web.AbstractControllerTest;
 import ru.restaurants.web.json.JsonUtil;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ru.restaurants.repository.UserDataTest.*;
+import static ru.restaurants.util.Convector.covertToUser;
 import static ru.restaurants.web.TestUtil.userHttpBasic;
 
 class ProfileRestControllerTest extends AbstractControllerTest {
@@ -24,8 +26,11 @@ class ProfileRestControllerTest extends AbstractControllerTest {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private VoteService voteService;
+
     @Test
-    void update() throws Exception {//переписать с тест матчером и сравнить возвращемый ти
+    void update() throws Exception {
         perform(MockMvcRequestBuilders.put(REST_UPDATE_PROFILE)
                 .with(userHttpBasic(USER_WITH_ID_15))
                 .contentType(MediaType.APPLICATION_JSON)
@@ -33,8 +38,10 @@ class ProfileRestControllerTest extends AbstractControllerTest {
                 .andExpect(status().isNoContent())
                 .andDo(print());
 
-        assertThat(userService.get(USER_ID_15).getName()).isEqualTo(UPDATE_USER_WITH_ROLE_USER.getName());
-        assertThat(userService.get(USER_ID_15).getEmail()).isEqualTo(UPDATE_USER_WITH_ROLE_USER.getEmail().toLowerCase());
+        User user = covertToUser(UPDATE_USER_WITH_ROLE_USER, voteService);
+        user.setEmail(user.getEmail().toLowerCase());
+
+        USER_TEST_MATCHER_IGNORE_ID_PASSWORD_VOTE.assertMatch(userService.get(USER_ID_15), user);
     }
 
     @Test
